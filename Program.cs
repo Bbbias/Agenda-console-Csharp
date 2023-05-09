61817a110e14bc8aa3863f368ddeca8f997f033a
Beatriz Cristiane Soares <91436573+Bbbias@users.noreply.github.com>
2023-05-09 16:56:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿List<Contato> listaDeContatos = new List<Contato>();
+var escolha = "0";
+
+while(escolha != "3"){
+
+        Console.WriteLine("Bem vindo a agenda! \nEscolha uma das opções: \n1 - Cadastrar \n2 - Listar \n3 - Sair");
+        escolha = Console.ReadLine();
+
+            if(escolha == "1"){
+                Console.WriteLine("Digite o nome do contato");
+                var nomeAdicionar = Console.ReadLine();
+                Console.WriteLine("Digite o numero do contato");
+                var telefoneAdicionar = Console.ReadLine();
+
+                Contato contatoAdicionar = new Contato();
+                contatoAdicionar.Nome = nomeAdicionar;
+                contatoAdicionar.Telefone = telefoneAdicionar;
+
+                listaDeContatos.Add(contatoAdicionar);
+
+            }
+
+            if(escolha == "2"){
+
+                Console.WriteLine("Lista de contatos:");
+                foreach(Contato contato in listaDeContatos){
+                Console.WriteLine(contato.Nome);
+                Console.WriteLine(contato.Telefone);
+                }
+
+            }
+   
+}
+
+    public class Contato
+    {
+        public string Nome { get; set; } 
+        public string Telefone { get; set; } 
+        public Contato()
+        { }
+
+        public Contato( string nome,string telefone)
+        {
+           Nome = nome;
+           Telefone = telefone;
+        }
+    }
+
+
+
+
+
+
+
+
+
+    
+
